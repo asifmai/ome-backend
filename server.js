24a94f7ce84e0d5ea95a0ad37ad769560e646330
hameed <vjhameed3@gmail.com>
2020-10-09 21:39:39 +0500

    fix

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const logger = require('morgan');
-const cors = require('cors');
-const config = require('./config/main');
+const logger = require("morgan");
+const cors = require("cors");
+const config = require("./config/main");
 
 // Connect to DB
-require('./helpers/db')();
+require("./helpers/db")();
 
 // Middlewares
-app.use(logger('dev'));
-app.use(express.urlencoded({ extended: false, limit: '100mb'}));
-app.use(express.json({extended: false, limit: '100mb'}));
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false, limit: "100mb" }));
+app.use(express.json({ extended: false, limit: "100mb" }));
 app.use(cors());
-
+app.use(express.static("public"));
 // Routes
-app.get('/', (req, res) => res.status(200).send('Server is Running...'));
-app.use('/api', require('./routes/api'));
+app.get("/", (req, res) => res.status(200).send("Server is Running..."));
+app.use("/api", require("./routes/api"));
 
 app.listen(config.port, () => {
   console.log(`Server listening on port ${config.port}...`);
