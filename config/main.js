0f510c13d19236f0c140509f4b35be0ecd8c40ac
vjhameed <vjhameed3@gmail.com>
2020-10-27 12:26:13 +0500

    fix

--- config/main.js
+++ config/main.js
@@ -1,14 +1,14 @@
 module.exports = {
   // secret key for jwt signing and encryption
-  'secret': process.env.JWT_SECRET,
+  secret: process.env.JWT_SECRET,
   // db connection info
-  'database': process.env.MONGODB_CONNECTION,
+  database: process.env.MONGODB_CONNECTION,
   // port for server
-  'port': process.env.PORT || 3000,
+  port: process.env.PORT || 5000,
 
   // fb sdk credentials
-  'fb': {
-    'appID' : process.env.FACEBOOK_APP_ID,
-    'appSecret' : process.env.FACEBOOK_APP_SECRET
-  }
+  fb: {
+    appID: process.env.FACEBOOK_APP_ID,
+    appSecret: process.env.FACEBOOK_APP_SECRET,
+  },
 };
